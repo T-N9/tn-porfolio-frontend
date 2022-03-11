d2f065932e2cf61b6158c6defaacea59003eab55
T-N9 <tenyainmoelwin@gmail.com>
2022-03-11 10:12:19 +0630

    Update project page

--- src/components/ProjectStatus/index.js
+++ src/components/ProjectStatus/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProjectStatus = (props) => {
 
-    const { status, url, period, role, type } = props;
+    const { status, period, role, type } = props;
 
     return (
         <section className='status container'>
