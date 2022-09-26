6458aea358db6be6abd46f04825cdba59bb39f82
tenyain <tenyain.mhs@gmail.com>
2022-09-26 08:10:03 +0630

    some minor updates

--- src/components/ContactHero/index.js
+++ src/components/ContactHero/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const ContactHero = () => {
     return (
@@ -37,9 +38,11 @@ const ContactHero = () => {
                                 </li>
                             </ul>
                         </div>
-                        <button className='secondary_btn'>
-                            My Services
-                        </button>
+                        <Link to="/services">
+                            <button className='secondary_btn'>
+                                My Services
+                            </button>
+                        </Link>
                     </div>
 
 
