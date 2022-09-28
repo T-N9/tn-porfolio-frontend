12fe4f0091243956003574409d095354802a12f0
tenyain <tenyain.mhs@gmail.com>
2022-09-28 08:53:10 +0630

    [LazyLoad] added

--- src/components/AboutHero/index.js
+++ src/components/AboutHero/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ImageLoader } from "render-img";
 
 /* Images */
 import HeroImage from "../../assets/aboutHero.webp";
@@ -11,13 +12,15 @@ const AboutHero = () => {
           <div className="about_hero--content">
             <h1 className="">
               Hi there, I am <span className="ht_text">Te Nyain</span> Moe Lwin,
-              21 years old creative <span className="ht_text">front-end</span> web developer with 1+ years experience. I am here to help
-              turn your <span className="ht_text">idea into life.</span>{" "}
+              21 years old creative <span className="ht_text">front-end</span>{" "}
+              web developer with 1+ years experience. I am here to help turn
+              your <span className="ht_text">idea into life.</span>{" "}
             </h1>
           </div>
 
           <div className="about_hero--image">
-            <img src={HeroImage} alt="" />
+            <ImageLoader src={HeroImage} lazyload alt="tenyain at UCSH" />
+            {/* <img  alt="" /> */}
           </div>
         </div>
       </div>
