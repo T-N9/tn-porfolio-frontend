3ad710f54b14560471fe9e4f7c67c0d8f17a3fa6
tenyain <tenyain.mhs@gmail.com>
2022-09-29 08:03:43 +0630

    [ReactLazy] added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import ScrollToTop from "./container/ScrollToTop";
 import { NavBar } from "./components";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentTheme } from "./redux/slices/NavbarSlice";
-import {
-  HomePage,
-  AboutPage,
-  ContactPage,
-  ProjectPage,
-  PortfolioPage,
-  ServicePage,
-  CreditPage
-} from "./pages";
 import "css.gg/icons/css/spinner.css";
+
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const AboutPage = React.lazy(() => import("./pages/AboutPage"));
+const ContactPage = React.lazy(() => import("./pages/ContactPage"));
+const ProjectPage = React.lazy(() => import("./pages/ProjectPage"));
+const PortfolioPage = React.lazy(() => import("./pages/PortfolioPage"));
+const ServicePage = React.lazy(() => import("./pages/ServicePage"));
+const CreditPage = React.lazy(() => import("./pages/CreditPage"));
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -49,17 +48,27 @@ const App = () => {
         <i className={`gg-${"spinner"}`}></i>
       </div>
 
-      <ScrollToTop>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/portfolio/:slug" element={<ProjectPage />} />
-          <Route path="/portfolio" element={<PortfolioPage />} />
-          <Route path="/services" element={<ServicePage />} />
-          <Route path="/credits" element={<CreditPage />} />
-        </Routes>
-      </ScrollToTop>
+      <Suspense
+        fallback={
+          <div
+            className={loading ? "loading_page visible" : "loading_page hidden"}
+          >
+            <i className={`gg-${"spinner"}`}></i>
+          </div>
+        }
+      >
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/portfolio/:slug" element={<ProjectPage />} />
+            <Route path="/portfolio" element={<PortfolioPage />} />
+            <Route path="/services" element={<ServicePage />} />
+            <Route path="/credits" element={<CreditPage />} />
+          </Routes>
+        </ScrollToTop>
+      </Suspense>
     </main>
   );
 };
