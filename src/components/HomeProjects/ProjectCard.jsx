51c4c868a3e4ea9ae265b40807d6b3f6e9c72510
tenyain <tenyain.mhs@gmail.com>
2022-10-16 15:44:14 +0630

    [render-img] test

--- src/components/HomeProjects/ProjectCard.jsx
+++ src/components/HomeProjects/ProjectCard.jsx
@@ -4,6 +4,12 @@ import "css.gg/icons/css/arrow-right-r.css";
 import "css.gg/icons/css/code.css";
 import "css.gg/icons/css/color-bucket.css";
 
+/* Images */
+import Placeholder from '../../assets/placeholder.webp';
+
+/* Component */
+import { ImageLoader } from "render-img";
+
 const ProjectCard = (props) => {
     const { title, desc, image, icon, category} = props;
     return (
@@ -19,7 +25,18 @@ const ProjectCard = (props) => {
                         </div>
                     </div>
                     <div className="projectCard--image">
-                        <img src={image} width="525px" height="339.66px" alt="portfolio project"/>
+                        <ImageLoader
+                        // placeHolderSrc={Placeholder}
+                        src={image}
+                        errorSrc={Placeholder}
+                        alt="portfolio project"
+                        width={525} // add Intrinsic width
+                        height={339.66} // Intrinsic height
+                        threshold={100} // default => 100
+                        isUseblur={true} // default => false
+                        // backgroundColor="#b4b4b4" // background for overlay
+                        />
+                        {/* <img src={image} width="525px" height="339.66px" alt="portfolio project"/> */}
                     </div>
                 </div>
                 <div className="col_2">
