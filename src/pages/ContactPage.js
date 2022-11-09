dc75520d6dda6edc384d11bd64e28d34bf9a9cda
tenyain <tenyain.mhs@gmail.com>
2022-11-09 14:10:30 +0630

    [Navigation] active state contact done

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setCurrentPage } from '../redux/slices/NavbarSlice';
+
+/* Components */
 import { HomeContact, Footer, ContactHero } from '../components';
 
 const ContactPage = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setCurrentPage('Contact'))
+    }, [])
+
     return (
         <>
             <div className="nav_spacer"></div>
