2f2e49d23eb9e75597326b89a6dc93ba0dde6079
tenyain <tenyain.mhs@gmail.com>
2022-11-09 14:48:31 +0630

    [Service Page] Hero-text updated

--- src/components/ServiceHero/index.js
+++ src/components/ServiceHero/index.js
@@ -5,13 +5,13 @@ const ServiceHero = () => {
     <section className="service_hero">
       <div>
         <div className="service_hero--overlay">
-
         </div>
+        <h1 style={{ marginBottom: "1rem", zIndex : "500", position : 'relative', textAlign: "center" }} className="title_text ht_text">My Services</h1>
         <h1 className="service_hero--text">
-          <span className="ht_text">Craft</span> and{" "}
-          <span className="ht_text">Build</span> Web
+          <span className="">Craft</span> and{" "}
+          <span className="">Build</span> Web
           <br />
-          beyond <span className="ht_text">creativity</span>.
+          beyond <span className="">creativity</span>.
         </h1>
 
         <a href="#services">
