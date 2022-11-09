570ef34a485d82ddbb04238a85502f37607854b0
tenyain <tenyain.mhs@gmail.com>
2022-11-09 14:08:14 +0630

    [Navigation] active state services done

--- src/pages/ServicePage.js
+++ src/pages/ServicePage.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setCurrentPage } from '../redux/slices/NavbarSlice';
 
 /* Components */
 import { ServiceHero, ServiceBody , Footer } from "../components";
 
 const ServicePage = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      dispatch(setCurrentPage('Services'))
+  }, [])
+
   return (
     <>
       <div className="nav_spacer"></div>
