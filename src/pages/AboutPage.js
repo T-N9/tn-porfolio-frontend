e9927f317ef3c59bc79ae75128885fd8032043fc
tenyain <tenyain.mhs@gmail.com>
2022-11-09 13:52:42 +0630

    [Navigation] active state About done

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setCurrentPage } from '../redux/slices/NavbarSlice';
 
 /* Components */
 import { AboutHero, AboutTimeline, Footer, HomeMotto, HomeAbout  } from "../components";
 
 const AboutPage = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      dispatch(setCurrentPage('About'))
+  }, [])
+
   return (
     <>
       <div className="nav_spacer"></div>
