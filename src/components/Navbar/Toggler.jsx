6aac2e6e7219b45857ed526dd4cfe75d16f53363
tenyain <tenyain.mhs@gmail.com>
2022-11-09 17:52:09 +0630

    [ThemeMode] icon added

--- src/components/Navbar/Toggler.jsx
+++ src/components/Navbar/Toggler.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setTheme } from "../../redux/slices/NavbarSlice";
+import "css.gg/icons/css/sun.css";
+import "css.gg/icons/css/moon.css";
 
 const Toggler = () => {
 
@@ -8,11 +10,16 @@ const Toggler = () => {
     const { theme } = useSelector((state) => state.navBar);
     localStorage.setItem('theme', theme);
 
+
     return (
         <>
             <button aria-label='toggle nav' onClick={() => dispatch(setTheme())} className='nav_toggler'>
                 <title>Change Theme</title>
+
+                <i className={`gg-${theme ? 'moon' : 'sun'}`}></i>
             </button>
+
+            
         </>
     )
 }
