9e849d3682fab015b847640b39da55003d89104f
tenyain <tenyain.mhs@gmail.com>
2022-11-09 14:26:58 +0630

    [WorksPage] hero-text updated

--- src/components/PortfolioHero/index.js
+++ src/components/PortfolioHero/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from "react";
 import "css.gg/icons/css/arrow-down.css";
 
 const PortfolioHero = () => {
-    return (
-        <>
-            <div className="hero_portfolio">
-                <div className="flex_auto gradient_bg_transparent">
-                    <h1>
-                        The <span className="ht_text">followings</span> are my practical 
-                        and personal projects.
-                    </h1>
+  return (
+    <>
+      <div className="hero_portfolio">
+        <div className="flex_auto gradient_bg_transparent">
+          <div className="hero_txt">
+            <h1 style={{ marginBottom: "1rem" }} className="title_text ht_text">
+              My Works
+            </h1>
+            <h1>The followings are my practical and personal projects.</h1>
+          </div>
 
-                    <a href="#projects">
-                        <button className="downToContent">
-                            <i className={`gg-${"arrow-down"}`}></i>
-                        </button>
-                    </a>
-                </div>
-            </div>
-        </>
-    );
-}
+          <a href="#projects">
+            <button className="downToContent">
+              <i className={`gg-${"arrow-down"}`}></i>
+            </button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default PortfolioHero;
