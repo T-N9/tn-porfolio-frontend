fb2edd7c42a8b219740a338869c7ceea387a3665
tenyain <tenyain.mhs@gmail.com>
2022-11-10 09:45:23 +0630

    [MobileNavItems] active-state done

--- src/components/Navbar/MobileMenu.jsx
+++ src/components/Navbar/MobileMenu.jsx
@@ -1,49 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import Hamburger from './Hamburger';
-import Toggler from './Toggler';
-import { setActive } from '../../redux/slices/NavbarSlice';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Hamburger from "./Hamburger";
+import Toggler from "./Toggler";
+import { setActive } from "../../redux/slices/NavbarSlice";
+
+const pages = [
+  {
+    name: "Home",
+    route: "/",
+  },
+  {
+    name: "About",
+    route: "/about",
+  },
+  {
+    name: "Works",
+    route: "/works",
+  },
+  {
+    name: "Services",
+    route: "/services",
+  },
+  {
+    name: "Contact",
+    route: "/contact",
+  },
+];
 
 const MobileMenu = () => {
+  const dispatch = useDispatch();
+  const { active, currentPage } = useSelector((state) => state.navBar);
+  const toggleClick = () => {
+    dispatch(setActive());
+  };
 
-    const dispatch = useDispatch();
-    const { active } = useSelector((state) => state.navBar);
-    const toggleClick = () => {
-        dispatch(setActive());
-    }
+  return (
+    <>
+      <section
+        className={active ? "nav_mobile_wrapper active" : "nav_mobile_wrapper"}
+      >
+        <Hamburger />
+        <ul className="nav_mobile_items">
+          {pages.map((page, index) => {
+            return (
+              <li
+                onClick={toggleClick}
+                key={index}
+                className={`nav_item link_hover ${
+                  currentPage === page.name && "ht_text"
+                }`}
+              >
+                <Link to={page.route}>{page.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
 
-    return (
-        <>
-            <section className={active ? "nav_mobile_wrapper active" : "nav_mobile_wrapper"}>
-                <Hamburger/>
-                <ul className="nav_mobile_items">
-                    <li onClick={toggleClick} className="nav_item">
-                        <Link to="/about">
-                            About
-                        </Link>
-                    </li>
-                    <li onClick={toggleClick} className="nav_item">
-                        <Link to="/works">
-                            Works
-                        </Link>
-                    </li>
-                    <li onClick={toggleClick} className="nav_item">
-                        <Link to="/services">
-                            Services
-                        </Link>
-                    </li>
-                    <li onClick={toggleClick} className="nav_item">
-                        <Link to="/contact">
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
-
-                <Toggler/>
-            </section>
-        </>
-    );
-}
+        <Toggler />
+      </section>
+    </>
+  );
+};
 
 export default MobileMenu;
