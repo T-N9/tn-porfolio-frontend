4dfd59345c834cb10324088714e1e0e295fd2a9b
tenyain <tenyain.mhs@gmail.com>
2022-11-28 21:52:45 +0630

    [Testimonial] Design Updated

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -19,20 +19,42 @@ const data = [
     position: "Instructor",
     company: "MMSIT",
     saying:
-      "Te Nyain is a well-trained student who took the \"Special Web Design\" course from us. He is specialized in Front-end development and delivering trustworthy interactive websites to his consumers.",
-  }
+      'Te Nyain is a well-trained student who took the "Special Web Design" course from us. He is specialized in Front-end development and delivering trustworthy interactive websites to his consumers.',
+  },
+  {
+    name: "Chris",
+    company: "LOR Media",
+    country: "USA",
+    position: "Business Owner",
+    saying:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium suscipit aliquid, aut eum sequi assumenda excepturi accusamus magnam quod doloremque? Omnis autem nesciunt ipsam deserunt debitis ex cumque obcaecati.",
+  },
+  {
+    name: "David",
+    country: "Myanmar",
+    position: "Front-end developer",
+    saying:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    name: "Michael",
+    country: "Japan",
+    position: "Senior Programmer",
+    saying:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
 ];
 
 const Testimonials = () => {
   return (
     <section className="testimonials">
       <div className="background_div">
-        <div className="container_sm">
+        <div className="">
           <h1 className="title_text text_center">What they say</h1>
           <p className="text_center desc_text">
             Things my mentors and seniors talk about me.
           </p>
-        
+
           <div className="card_wrapper">
             <Swiper
               spaceBetween={30}
@@ -44,20 +66,17 @@ const Testimonials = () => {
                 disableOnInteraction: false,
               }}
               loop={true}
-              modules={[FreeMode, Pagination, Autoplay]}
+              modules={[FreeMode, Pagination , Autoplay]}
               className="mySwiper"
               breakpoints={{
                 640: {
                   slidesPerView: 1,
-        
                 },
                 768: {
                   slidesPerView: 1,
-        
                 },
                 1024: {
                   slidesPerView: 2,
-        
                 },
               }}
             >
