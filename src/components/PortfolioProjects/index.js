3371dbed4299bbe188641f28cd4339810a15ad0e
tenyain <tenyain.mhs@gmail.com>
2022-11-29 17:45:04 +0630

    [ProjectPortfolio] redux-state

--- src/components/PortfolioProjects/index.js
+++ src/components/PortfolioProjects/index.js
@@ -1,48 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { nanoid } from '@reduxjs/toolkit';
-import ProjectCard from '../HomeProjects/ProjectCard';
-import { Link } from 'react-router-dom';
-import { client, urlFor } from '../../client';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import ProjectCard from "../HomeProjects/ProjectCard";
+import { Link } from "react-router-dom";
+import { client, urlFor } from "../../client";
+
+/* Action */
+import { setProjectData } from "../../redux/slices/ProjectsSlice";
 
 const PortfolioProjects = () => {
+  const [projects, setProjects] = useState(null);
+  const dispatch = useDispatch();
+  const { contentData } = useSelector((state) => state.projectData);
 
-    const [ projects, setProjects ] = useState(null);
+  useEffect(() => {
+    const query = '*[_type == "projects"] | order(order asc)';
 
-    useEffect(()=> {
-        const query = '*[_type == "projects"] | order(order asc)';
-    
-        client.fetch(query).then((data) => {
-          setProjects(data);
-        });
-    }, [])
+    contentData.length === 0 &&
+      client.fetch(query).then((data) => {
+        setProjects(data);
+        dispatch(setProjectData(data));
+      });
+  }, []);
 
-    let projectList;
-    if(projects !== null) {
-        projectList = projects.map( project => {
-            const { title, description , icon, category, slug} = project;
-            return (
-                <Link key={nanoid()} to={`/works/${slug.current}`}>
-                    <ProjectCard
-                        title = {title}
-                        desc = {description}
-                        image = {urlFor(project.imgUrl)}
-                        icon = {icon}
-                        category = {category}
-                    />
-                </Link>
-            );  
-        });
-    }
+  let projectList;
+  if (contentData !== null) {
+    projectList = contentData?.map((project) => {
+      const { title, description, icon, category, slug } = project;
+      return (
+        <Link key={nanoid()} to={`/works/${slug.current}`}>
+          <ProjectCard
+            title={title}
+            desc={description}
+            image={urlFor(project.imgUrl)}
+            icon={icon}
+            category={category}
+          />
+        </Link>
+      );
+    });
+  }
 
-    return (
-        <div id="projects" className="projectContent container">
-            {/* <span ></span> */}
-            <h1 className="title_text">Recent Works</h1>
-            <div className="projectContent--grid">
-                {projectList}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div id="projects" className="projectContent container">
+      {/* <span ></span> */}
+      <h1 className="title_text">Recent Works</h1>
+      <div className="projectContent--grid">{projectList}</div>
+    </div>
+  );
+};
 
 export default PortfolioProjects;
