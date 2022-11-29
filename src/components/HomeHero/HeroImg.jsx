cd05437f5e9651433e364a135e54ab76e9b8889d
tenyain <tenyain.mhs@gmail.com>
2022-11-29 10:10:39 +0630

    [HomeHero] updated

--- src/components/HomeHero/HeroImg.jsx
+++ src/components/HomeHero/HeroImg.jsx
@@ -9,7 +9,7 @@ const HeroImg = () => {
                     <img className="hero_img--image" src={heroImg} alt="tenyain with a plane" loading='lazy'/>
                     <span className="squares square_1"></span>
                     <span className="squares square_2"></span>
-                    <p>トリウム浜田</p>
+                    <p className='jp-code'>トリウム浜田</p>
                 </div>
 
             </div>
