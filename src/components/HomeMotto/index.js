51755fdd232033e47f0aaef4491091b3e86f9a82
tenyain <tenyain.mhs@gmail.com>
2022-11-29 23:01:18 +0630

    [Motto] Updated

--- src/components/HomeMotto/index.js
+++ src/components/HomeMotto/index.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import Bricks from './Bricks';
+import React from "react";
+import Bricks from "./Bricks";
 
 const HomeMotto = () => {
-    return (
-        <section className="motto_page_wrapper">
-            <div className="motto_page">
-                <h1>
-                    “ If I cannot build a wall,
-                      I will lay a brick every day
-                      brick by brick until
-                      it becomes a wall.
-                    “    
-                </h1>
+  return (
+    <section className="motto_page_wrapper">
+      <div className="motto_page">
+        <h1>
+          “ If I cannot build a wall, I will lay a brick every day one by one
+          until it becomes a wall. “
+        </h1>
 
-                <div className="bricks">
-                    <Bricks/>
-                </div>
-            </div>
-        </section>
-    );
-}
+        <div className="bricks">
+          <Bricks />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default HomeMotto;
