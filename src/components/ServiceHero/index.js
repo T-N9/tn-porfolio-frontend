5ea3dd52edad1eba37c961a446cdd2c4f2452471
tenyain <tenyain.mhs@gmail.com>
2022-12-07 19:33:16 +0630

    [ServiceHero] updated bg

--- src/components/ServiceHero/index.js
+++ src/components/ServiceHero/index.js
@@ -3,16 +3,16 @@ import React from "react";
 const ServiceHero = () => {
   return (
     <section className="service_hero">
-      <div>
-        <div className="service_hero--overlay">
+      <div className="container_sm gradient_bg_transparent">
+        <div>
+          <h1 style={{ marginBottom: "1rem", zIndex : "500", position : 'relative', textAlign: "center" }} className="title_text ht_text">My Services</h1>
+          <h1 className="service_hero--text">
+            <span className="">Craft</span> and{" "}
+            <span className="">Build</span> Web
+            <br />
+            beyond <span className="">creativity</span>.
+          </h1>
         </div>
-        <h1 style={{ marginBottom: "1rem", zIndex : "500", position : 'relative', textAlign: "center" }} className="title_text ht_text">My Services</h1>
-        <h1 className="service_hero--text">
-          <span className="">Craft</span> and{" "}
-          <span className="">Build</span> Web
-          <br />
-          beyond <span className="">creativity</span>.
-        </h1>
 
         <a href="#services">
           <button className="downToContent"><i className="gg-arrow-down"></i></button>
